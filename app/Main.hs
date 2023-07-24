a4d81a31ac77ebce562a8be3bf179b79c42d96ca
andrii <25188+unorsk@users.noreply.github.com>
2023-07-24 15:10:25 +0200

    parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,39 @@
 module Main where
 
-import MyLib qualified (someFunc)
+-- import MyLib qualified (someFunc)
+
+import NaiveParser (Error)
+import System.Console.Haskeline
+  ( InputT
+  , defaultSettings
+  , getInputLine
+  , outputStrLn
+  , runInputT
+  )
+
+data Some = Error
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main =
+  runInputT defaultSettings loop
+ where
+  loop :: InputT IO ()
+  loop = do
+    minput <- getInputLine "yaq> "
+    case minput of
+      Nothing -> return ()
+      Just "quit" -> return ()
+      Just input -> do
+        outputStrLn $ "Input was: " ++ input
+        loop
+
+-- MyLib.someFunc
+
+-- yaq>
+-- q)a:42
+-- q)a :42
+-- q)a: 42
+-- q)a : 42
+-- q)a
+-- q)c
+-- c is not defined
